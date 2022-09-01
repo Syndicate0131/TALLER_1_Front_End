c86f78c99fa62d80702729f8a4014be93807d4b8
Syndicate <jdavidquitiansaa@gmail.com>
2022-08-31 21:15:16 -0500

    Ejercicio Cuestionario

---
+++ js/cuestionario.js
@@ -0,0 +1,63 @@
+alert("Bienvenido, este programa evaluara su conocimiento sobre los instructores del trimestre y sus materias")
+
+function eval() {
+    let nom=document.getElementById('nom').value;
+    let ape=document.getElementById('ape').value;
+    let mat=document.getElementById('mat').value;
+    if (nom==1) {
+        if (ape==1 && mat==1) {
+            alert("Respuesta Correcta!");
+        }
+        else{
+            alert("Respuesta Incorrecta!");
+        }
+    }
+    if (nom==2) {
+        if (ape==2 && mat==2) {
+            alert("Respuesta Correcta!");
+        }
+        else{
+            alert("Respuesta Incorrecta!");
+        }
+    }
+    if (nom==3) {
+        if (ape==3 && mat==3) {
+            alert("Respuesta Correcta!");
+        }
+        else{
+            alert("Respuesta Incorrecta!");
+        }
+    }
+    if (nom==4) {
+        if (ape==4 && mat==4) {
+            alert("Respuesta Correcta!");
+        }
+        else{
+            alert("Respuesta Incorrecta!");
+        }
+    }
+    if (nom==5) {
+        if (ape==5 && mat==5) {
+            alert("Respuesta Correcta!");
+        }
+        else{
+            alert("Respuesta Incorrecta!");
+        }
+    }
+    if (nom==6) {
+        if (ape==6 && mat==6) {
+            alert("Respuesta Correcta!");
+        }
+        else{
+            alert("Respuesta Incorrecta!");
+        }
+    }
+    if (nom==7) {
+        if (ape==7 && mat==7) {
+            alert("Respuesta Correcta!");
+        }
+        else{
+            alert("Respuesta Incorrecta!");
+        }
+    }
+}
